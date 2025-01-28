a30319bca8a48e3779264a68e6ca0165c69c3c53
Fernando Chanatasig <chanatasigluis2@gmail.com>
2025-01-28 11:27:51 -0500

    hasta aqui todo bien

--- ExpertMed/Controllers/ConsultationController.cs
+++ ExpertMed/Controllers/ConsultationController.cs
@@ -92,29 +92,160 @@ namespace ExpertMed.Controllers
         }
 
 
-        [HttpPost]
-        public IActionResult CreateConsultation([FromBody] ConsultationDTO consultation)
-        {
-            try
-            {
-                // Validación básica
-                if (consultation == null)
-                {
-                    return BadRequest(new { message = "Consulta no válida." });
-                }
+        //[HttpPost("Crear-Consulta")]
+        //public async Task<IActionResult> CrearConsulta([FromBody] Consulta consultaDto)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        var errores = ModelState
+        //            .Where(x => x.Value.Errors.Count > 0)
+        //            .Select(x => new { Field = x.Key, Errors = x.Value.Errors.Select(e => e.ErrorMessage) })
+        //            .ToList();
 
-                // Llamada al servicio para crear la consulta
-                _consultationService.CreateConsultation(consultation);
+        //        foreach (var error in errores)
+        //        {
+        //            Console.WriteLine($"Campo: {error.Field}, Errores: {string.Join(", ", error.Errors)}");
+        //        }
 
-                // Respuesta exitosa
-                return Ok(new { message = "Consulta creada exitosamente." });
-            }
-            catch (Exception ex)
-            {
-                // Manejo de errores
-                return StatusCode(500, new { message = "Ocurrió un error al crear la consulta.", error = ex.Message });
-            }
-        }
+        //        return Json(new { success = false, errores });
+        //    }
+
+
+
+        //    try
+        //    {
+        //        // Llama al servicio para crear la consulta
+        //        await _consultationService.CrearConsultaAsync(
+        //            consultaDto.ConsultationUsercreate,
+        //            consultaDto.ConsultationHistoryclinic,
+        //            consultaDto.ConsultationPatient,
+        //            consultaDto.ConsultationReason,
+        //            consultaDto.ConsultationDisease,
+        //            consultaDto.ConsultationFamiliaryname,
+        //            consultaDto.ConsultationWarningsings,
+        //            consultaDto.ConsultationNonpharmacologycal,
+        //            consultaDto.ConsultationFamiliarytype,
+        //            consultaDto.ConsultationFamiliaryphone,
+        //            consultaDto.ConsultationTemperature,
+        //            consultaDto.ConsultationRespirationrate,
+        //            consultaDto.ConsultationBloodpressuredAs,
+        //            consultaDto.ConsultationBloodpresuredDis,
+        //            consultaDto.ConsultationPulse,
+        //            consultaDto.ConsultationWeight,
+        //            consultaDto.ConsultationSize,
+        //            consultaDto.ConsultationTreatmentplan,
+        //            consultaDto.ConsultationObservation,
+        //            consultaDto.ConsultationPersonalbackground,
+        //            consultaDto.ConsultationDisablilitydays,
+        //            consultaDto.ConsultationUsercreate,
+        //            consultaDto.ConsultationSpeciality,
+        //            consultaDto.ConsultationType ?? 0,
+        //            consultaDto.ConsultationStatus,
+        //            // Parámetros de órganos y sistemas
+        //            consultaDto.OrgansSystem.sense,
+        //            consultaDto.OrganosSistemas.ObserOrgSentidos,
+        //            consultaDto.OrganosSistemas.Respiratorio,
+        //            consultaDto.OrganosSistemas.ObserRespiratorio,
+        //            consultaDto.OrganosSistemas.CardioVascular,
+        //            consultaDto.OrganosSistemas.ObserCardioVascular,
+        //            consultaDto.OrganosSistemas.Digestivo,
+        //            consultaDto.OrganosSistemas.ObserDigestivo,
+        //            consultaDto.OrganosSistemas.Genital,
+        //            consultaDto.OrganosSistemas.ObserGenital,
+        //            consultaDto.OrganosSistemas.Urinario,
+        //            consultaDto.OrganosSistemas.ObserUrinario,
+        //            consultaDto.OrganosSistemas.MEsqueletico,
+        //            consultaDto.OrganosSistemas.ObserMEsqueletico,
+        //            consultaDto.OrganosSistemas.Endocrino,
+        //            consultaDto.OrganosSistemas.ObserEndocrino,
+        //            consultaDto.OrganosSistemas.Linfatico,
+        //            consultaDto.OrganosSistemas.ObserLinfatico,
+        //            consultaDto.OrganosSistemas.Nervioso,
+        //            consultaDto.OrganosSistemas.ObserNervioso,
+        //            // Parámetros de examen físico
+        //            consultaDto.ExamenFisico.Cabeza,
+        //            consultaDto.ExamenFisico.ObserCabeza,
+        //            consultaDto.ExamenFisico.Cuello,
+        //            consultaDto.ExamenFisico.ObserCuello,
+        //            consultaDto.ExamenFisico.Torax,
+        //            consultaDto.ExamenFisico.ObserTorax,
+        //            consultaDto.ExamenFisico.Abdomen,
+        //            consultaDto.ExamenFisico.ObserAbdomen,
+        //            consultaDto.ExamenFisico.Pelvis,
+        //            consultaDto.ExamenFisico.ObserPelvis,
+        //            consultaDto.ExamenFisico.Extremidades,
+        //            consultaDto.ExamenFisico.ObserExtremidades,
+        //            // Parámetros de antecedentes familiares
+        //            consultaDto.AntecedentesFamiliares.Cardiopatia,
+        //            consultaDto.AntecedentesFamiliares.ObserCardiopatia,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoCardiopatia,
+        //            consultaDto.AntecedentesFamiliares.Diabetes,
+        //            consultaDto.AntecedentesFamiliares.ObserDiabetes,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoDiabetes,
+        //            consultaDto.AntecedentesFamiliares.EnfCardiovascular,
+        //            consultaDto.AntecedentesFamiliares.ObserEnfCardiovascular,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoEnfcardiovascular,
+        //            consultaDto.AntecedentesFamiliares.Hipertension,
+        //            consultaDto.AntecedentesFamiliares.ObserHipertension,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoHipertension,
+        //            consultaDto.AntecedentesFamiliares.Cancer,
+        //            consultaDto.AntecedentesFamiliares.ObserCancer,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoCancer,
+        //            consultaDto.AntecedentesFamiliares.Tuberculosis,
+        //            consultaDto.AntecedentesFamiliares.ObserTuberculosis,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoTuberculosis,
+        //            consultaDto.AntecedentesFamiliares.EnfMental,
+        //            consultaDto.AntecedentesFamiliares.ObserEnfMental,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoEnfmental,
+        //            consultaDto.AntecedentesFamiliares.EnfInfecciosa,
+        //            consultaDto.AntecedentesFamiliares.ObserEnfInfecciosa,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoEnfinfecciosa,
+        //            consultaDto.AntecedentesFamiliares.MalFormacion,
+        //            consultaDto.AntecedentesFamiliares.ObserMalFormacion,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoMalformacion,
+        //            consultaDto.AntecedentesFamiliares.Otro,
+        //            consultaDto.AntecedentesFamiliares.ObserOtro,
+        //            consultaDto.AntecedentesFamiliares.ParentescocatalogoOtro,
+        //            // Tablas relacionadas
+        //            consultaDto.Alergias,
+        //            consultaDto.Cirugias,
+        //            consultaDto.Medicamentos,
+        //            consultaDto.Laboratorios,
+        //            consultaDto.Imagenes,
+        //            consultaDto.Diagnosticos
+        //        );
+
+        //        // Devuelve un JSON con éxito si es una solicitud AJAX
+        //        if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+        //        {
+        //            TempData["SuccessMessage"] = $"La consulta creado exitosamente";
+
+        //            return Json(new { success = true, message = "Consulta creada exitosamente" });
+
+        //        }
+        //        else
+        //        {
+        //            // Redirigir en caso de una solicitud normal (no AJAX)
+        //            TempData["SuccessMessage"] = "Consulta Registrado.";
+        //            return Json(new { success = true, redirectUrl = Url.Action("ListarConsultas") });
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["ErrorMessage"] = $"Error al crear el paciente: {ex.Message}";
+
+        //        _logger.LogError(ex, "Error al crear la consulta");
+
+        //        if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+        //        {
+        //            return Json(new { success = false, message = "Ocurrió un error en el servidor.", details = ex.Message });
+        //        }
+        //        else
+        //        {
+        //            return View("Error", new ErrorViewModel { RequestId = ex.Message });
+        //        }
+        //    }
+        //}
 
     }
 }
